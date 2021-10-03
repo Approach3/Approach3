aba2fe1469fc83ae20f1040b9cd947b0532ce0cb
Approach3 <91844055+Approach3@users.noreply.github.com>
2021-10-03 22:01:37 +0330

    Delete test/1 directory

--- test/1/script.js
+++
@@ -1,9 +0,0 @@
-let countEl = document.getElementById("countl");
-let count = 0;
-function inc() {
-   count = count + 1;
-   countEl.innerText = count;
-}
-function save() {
-   console.log(count);
-}
